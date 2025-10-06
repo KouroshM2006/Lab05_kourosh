daee4a267e9a7efc8917c8bf249b2de81d0814b9
Kourosh Maissamyazad <2440557@edu.vaniercollege.qc.ca>
2025-10-06 10:50:30 -0400

    added slider

--- src/lab05_kourosh/task02.java
+++ src/lab05_kourosh/task02.java
@@ -7,7 +7,15 @@ package lab05_kourosh;
 import java.util.HashMap;
 import java.util.Map;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -16,15 +24,24 @@ import javafx.stage.Stage;
  * @author 2440557
  */
 public class task02 extends Application {
+
     private Map<String, Double> prices = new HashMap<>();
     
     public static void main(String[] args) {
         launch(args);
     }
-
+    
     @Override
     public void start(Stage primaryStage) {
         BorderPane root = new BorderPane();
+        GridPane grid = new GridPane();
+        
+        Slider slider = new Slider(0, 20, 0);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+        slider.setOrientation(Orientation.HORIZONTAL);
+        
+        Label sliderValueLabel = new Label("Tip: 0%");
         
         String[] itemNames = {
             "Coffee", "Tea", "Soft Drink", "Water", "Milk", "Juice",
@@ -33,7 +50,7 @@ public class task02 extends Application {
             "Shrimp Scampi", "Pasta", "Fish and Chips",
             "Apple Pie", "Carrot Cake", "Mud Pie", "Pudding", "Apple Crisp"
         };
-
+        
         double[] itemPrices = {
             2.50, 2.00, 1.75, 2.95, 1.50, 2.50,
             4.50, 3.75, 5.25, 3.00, 6.95,
@@ -53,8 +70,8 @@ public class task02 extends Application {
         
         ComboBox<String>[] comboBoxes = new ComboBox[]{beverageBox, appetizerBox, mainCourseBox, dessertBox};
         
+        String s = "Coffee";
         for (String item : itemNames) {
-            String s = "Coffee";
             
             if (item.equals("Soup")) {
                 s = item;
@@ -69,16 +86,29 @@ public class task02 extends Application {
             } else if (s.equals("Steak")) {
                 mainCourseBox.getItems().add(item);
             } else if (s.equals("Apple Pie")) {
-                dessertBox.getItems().add(item); 
+                dessertBox.getItems().add(item);                
             } else {
                 beverageBox.getItems().add(item);
             }
         }
         
-        HBox hBox = new HBox(beverageBox, appetizerBox, mainCourseBox, dessertBox);
+        grid.add(slider, 0, 0);
+        grid.add(sliderValueLabel, 0, 1);
+        grid.setAlignment(Pos.CENTER);
+        
+        HBox hBox = new HBox(new Label("Beverage:"), beverageBox, new Label("Appetizer:"), appetizerBox, new Label("Main Course:"), mainCourseBox, new Label("Dessert:"), dessertBox);
+        hBox.setSpacing(5);
+        root.setTop(hBox);
+        root.setCenter(slider);
+        root.setPadding(new Insets(25, 25, 25, 25));
         
         
+        
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
         
     }
-
+    
 }
