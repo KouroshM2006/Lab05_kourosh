aa0a90714bf8e1423ea1e717e54044e957feeb45
Kourosh Maissamyazad <2440557@edu.vaniercollege.qc.ca>
2025-10-06 09:54:29 -0400

    started task 2

---
+++ src/lab05_kourosh/task02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab05_kourosh;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2440557
+ */
+public class task02 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) {
+        
+    }
+    
+}
