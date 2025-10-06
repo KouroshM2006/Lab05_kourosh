fd4b6aca8b9a88abe54e3c5d48e7a165e37ba821
Kourosh Maissamyazad <2440557@edu.vaniercollege.qc.ca>
2025-10-06 08:59:50 -0400

    implemented root border pane, grid pane, list view and radio buttons

--- src/lab05_kourosh/Lab05_kourosh.java
+++ src/lab05_kourosh/Lab05_kourosh.java
@@ -4,7 +4,20 @@
  */
 package lab05_kourosh;
 
+
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -22,7 +35,55 @@ public class Lab05_kourosh extends Application{
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
+        
+        //creating root border pane
+        BorderPane root = new BorderPane();
+        
+        //creating labels
+        Label bagStyleLbl = new Label("Select Bag Style");
+        Label quantityLabel = new Label("Select Quantity:");
+        
+        
+        String[] bags = {"Full Decorative", "Beaded", "Pirate Design", "Fringed", "Leather", "Plain"};
+        ListView<String> listView = new ListView<>();
+        listView.setItems(FXCollections.observableArrayList(bags));
+        listView.setPrefSize(300, 300);
+        
+        //creating combo box
+        ComboBox<Integer> comboBox = new ComboBox<>();
+        comboBox.getItems().addAll(1,2,3,4,5,6,7,8,9,10);
+        
+        RadioButton smallRadioButton = new RadioButton("Small");
+        RadioButton medRadioButton = new RadioButton("Medium");
+        RadioButton largeRadioButton = new RadioButton("Large");
+        
+        ToggleGroup toggleGroup = new ToggleGroup();
+        smallRadioButton.setToggleGroup(toggleGroup);
+        medRadioButton.setToggleGroup(toggleGroup);
+        largeRadioButton.setToggleGroup(toggleGroup);
+        
+        
+        
+        
+        //adding nodes to root
+        root.setCenter(listView);
+        root.setLeft(bagStyleLbl);
+        root.setRight(vBox);
+        
+        
+        
+        //creating grid pane
+        GridPane grid = new GridPane();
+        
+        //setting scene 
+        Scene scene = new Scene(root);
+        
+        
+        primaryStage.setTitle("Bag Order Form");
+        primaryStage.setScene(scene); 
+        
+        primaryStage.show();
         
     }
     
